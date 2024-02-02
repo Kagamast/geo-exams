3afde2e852ea8c474770d58db77025a3b9ab4d21
Kagamast <87300175+Kagamast@users.noreply.github.com>
2024-02-02 15:22:21 +0100

    fixed first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import HomePage from './components/home/homePage.component';
+import GoToPage from './components/goTo/goToPage.component';
+import PlayPage from './components/playPage/playPage.component';
+import PageNotFound from './components/pageNotFound/pageNotFound.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <HomePage/>
+    // </div>
+    <Routes>
+      <Route index element={<HomePage/>}/>
+      <Route path='goto/*' element={<GoToPage/>}/>
+      <Route path='play/*' element={<PlayPage/>}/>
+      <Route path='/*' element={<PageNotFound/>}/>
+    </Routes>
   );
 }
 
